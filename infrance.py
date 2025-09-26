e75baaced916b5f4a4783afc1c474ebdba275e59
OrkhanEyvazov1 <orkhane50616@sabah.edu.az>
2025-09-26 21:56:13 +0400

    code

---
+++ infrance.py
@@ -0,0 +1,12 @@
+import pickle
+import numpy as np
+
+model= pickle.load(open('mo.pkl', 'rb'))
+
+def inference(user_data):
+    # Dummy inference functionexit
+    return model.predict(user_data)
+
+sample_user_data = [1996, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 197.0, 5.0]
+
+print(inference(sample_user_data))
