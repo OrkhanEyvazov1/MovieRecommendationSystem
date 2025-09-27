4140ae0b3b87489bf8e47f8cd6225a142ea74762
Orkhan Eyvazov <orkhane50616@sabah.edu.az>
2025-09-27 19:14:05 +0400

    Change model file name to 'catboost_model.pkl'

--- infrance.py
+++ infrance.py
@@ -1,7 +1,7 @@
 import pickle
 import numpy as np
 
-model= pickle.load(open('mo.pkl', 'rb'))
+model= pickle.load(open('catboost_model.pkl', 'rb'))
 
 def inference(user_data):
     # Dummy inference functionexit
